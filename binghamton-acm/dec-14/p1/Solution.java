ee430444ca8f60c696fe81d9d6d024cde6679b53
donth77 <donth77@gmail.com>
2015-02-26 00:03:20 -0500

    Update and rename binghamton-acm/dec-14 to binghamton-acm/dec-14/p1/Solution.java

---
+++ binghamton-acm/dec-14/p1/Solution.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int lineNum = 0;
+        Scanner input = new Scanner(System.in);
+        
+        while(input.hasNext()){
+            String line = input.next();
+            int value = Integer.parseInt(line);
+            outputPattern(value);
+        }
+    }
+    
+    public static void outputPattern(int val){
+        String pattern = "1";
+        
+        
+        for(int i = 0; i < val; i++){
+            int count = 0;
+            
+            System.out.println(pattern);
+            String newPattern = "";
+            
+            for (int j = 0; j < pattern.length(); j++){
+                char c = pattern.charAt(j); 
+                
+                if(j + 1 < pattern.length()){
+                    char nextC = pattern.charAt(j + 1);
+                        if(c == nextC){
+                            count++;
+                        }else{
+                            count++;
+                            newPattern = newPattern.concat(count + "" + c);
+                            count = 0;
+                        }
+                }else{
+                    count++;
+                    newPattern = newPattern.concat(count + "" + c);
+                    
+                }
+                
+             }
+            
+            pattern = newPattern;
+        }
+    }
+}
