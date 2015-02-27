7a9dc955820443fb5f4572f399f531438e613d70
donth77 <donth77@gmail.com>
2015-02-27 00:28:34 -0500

    Update and rename binghamton-acm to binghamton-acm/feb-15/p5/Solution.java

---
+++ binghamton-acm/feb-15/p5/Solution.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+	static int A = 0, B = 0;
+
+    public static void main(String[] args) {
+        int lineNum = 0;
+        Scanner input = new Scanner(System.in);
+        
+        while(input.hasNext()){
+            String line = input.nextLine(); 
+            String[] tokens = line.split("\\s+");
+            isNarcNumber(tokens[0]);
+        }
+    }
+	
+	public static void isNarcNumber(String number){
+       long value = Long.parseLong(number);
+       int k = number.length();
+       long sum = 0;
+	   for (int i = 0; i < number.length(); i++){
+            char c = number.charAt(i);        
+            long digitValue = Integer.parseInt(String.valueOf(c));
+            sum += Math.pow(digitValue, k);
+        }
+        
+        if(sum == value){
+            System.out.println("True");
+        }else{
+            System.out.println("False");
+        }
+	}
+}
